49388c0574c40349f00842af102bf11b58d22242
hrishabhsaxena <hrishabhsaxena131@gmail.com>
2026-09-12 18:04:49 +0530

    Import BackgroundMusic component in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-
+import BackgroundMusic from "@/components/BackgroundMusic";
   typescript: {
     ignoreBuildErrors: true,
   },
